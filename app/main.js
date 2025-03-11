0da603b6b9d2d060bdc43922fafab17d1146c802
Robledo Pazotto <robledo@gmail.com>
2025-03-11 11:08:42 -0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,14 +1,24 @@
 import {createServer} from "net";
 
 const server = createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-  });
-  
+    socket.on("close", () => {
+        socket.end();
+    });
+
     socket.on("data", (data) => {
-        console.log("RECEIVED: ", data.toString());
-        
-        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        console.log("RECEIVED:", data.toString());
+
+        const lines = data.toString().split("\r\n");
+        const firstLine = lines[0];
+        const [method, path, protocol] = firstLine.split(" ");
+
+        console.log("METHOD:", method, "PATH:", path, "PROTOCOL:", protocol);
+
+        if (path === "/") {
+            socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        } else {
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
     });
 });
 
