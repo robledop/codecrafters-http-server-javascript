e86c1fb8bc205f6a14236cc2ef5472dd4e9583a1
Robledo Pazotto <robledo@gmail.com>
2025-03-11 12:01:02 -0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 import {createServer} from "net";
-import {readFileSync, existsSync} from "fs";
+import {readFileSync, existsSync, writeFileSync} from "fs";
 
 let directory = null;
 if (process.argv.length === 4 && process.argv[2] === "--directory") {
@@ -31,31 +31,46 @@ const server = createServer((socket) => {
         console.log(`METHOD: ${method}, PATH: ${path}, PROTOCOL: ${protocol}`);
 
         let response;
-        if (path === "/") {
-            response = "HTTP/1.1 200 OK\r\n\r\n";
-        } else if (path.startsWith("/echo/")) {
-            const message = path.substring(6);
-            response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`;
-        } else if (path === "/user-agent") {
-            const userAgentLine = lines.find(x => x.startsWith("User-Agent: "));
-            const userAgent = userAgentLine.substring(12);
-            response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
-        } else if (path.startsWith("/files/")) {
-            if (directory === null) {
-                response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        if (method === "GET") {
+            if (path === "/") {
+                response = "HTTP/1.1 200 OK\r\n\r\n";
+            } else if (path.startsWith("/echo/")) {
+                const message = path.substring(6);
+                response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`;
+            } else if (path === "/user-agent") {
+                const userAgentLine = lines.find(x => x.startsWith("User-Agent: "));
+                const userAgent = userAgentLine.substring(12);
+                response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
+            } else if (path.startsWith("/files/")) {
+                if (directory === null) {
+                    response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                } else {
+                    const file_name = path.substring(7);
+                    const file_path = `${directory}/${file_name}`;
+                    if (existsSync(file_path)) {
+                        const file = readFileSync(file_path);
+                        response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${file.length}\r\n\r\n${file}`;
+                    } else {
+                        response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                    }
+                }
             } else {
+                response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            }
+        } else if (method === "POST") {
+            if (path.startsWith("/files/")) {
+                const contentLengthLine = lines.find(x => x.startsWith("Content-Length: "));
+                const contentLength = parseInt(contentLengthLine.substring(16));
+
+                const body = data.toString().substring(data.toString().indexOf("\r\n\r\n") + 4);
                 const file_name = path.substring(7);
                 const file_path = `${directory}/${file_name}`;
-                if (existsSync(file_path)) {
-                    const file = readFileSync(file_path);
-                    response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${file.length}\r\n\r\n${file}`;
-                } else {
-                    response = "HTTP/1.1 404 Not Found\r\n\r\n";
-                }
+                
+                console.log(`Writing to ${file_path}, Content: ${body}`);
+                writeFileSync(file_path, body);
+                
+                response = "HTTP/1.1 201 Created\r\n\r\n";
             }
-
-        } else {
-            response = "HTTP/1.1 404 Not Found\r\n\r\n";
         }
 
         socket.write(response);
