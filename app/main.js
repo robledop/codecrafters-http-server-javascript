e1bdbf7de0f9dd3f04202bbe5960e567ee121731
Robledo Pazotto <robledo@gmail.com>
2025-03-11 11:18:50 -0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -12,10 +12,15 @@ const server = createServer((socket) => {
         const firstLine = lines[0];
         const [method, path, protocol] = firstLine.split(" ");
 
-        console.log("METHOD:", method, "PATH:", path, "PROTOCOL:", protocol);
+        console.log(`METHOD: ${method}, PATH: ${path}, PROTOCOL: ${protocol}`);
 
         if (path === "/") {
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        } else if (path.startsWith("/echo/")) {
+            const message = path.substring(6);
+            const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`;
+
+            socket.write(response);
         } else {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         }
