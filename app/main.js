f3e476363af702bb433b4d46870c959cae82bfe6
Robledo Pazotto <robledo@gmail.com>
2025-03-11 11:26:50 -0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -3,7 +3,14 @@ import {createServer} from "net";
 const server = createServer((socket) => {
     socket.on("close", () => {
         socket.end();
+        socket.close();
     });
+
+    socket.on("error", (err) => {
+        console.error(err);
+        socket.end();
+        socket.close();
+    })
 
     socket.on("data", (data) => {
         console.log("RECEIVED:", data.toString());
@@ -20,10 +27,18 @@ const server = createServer((socket) => {
         } else if (path.startsWith("/echo/")) {
             const message = path.substring(6);
             response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`;
+        } else if (path === "/user-agent") {
+            lines.forEach((line) => {
+                if (line.startsWith("User-Agent: ")) {
+                    const userAgent = line.substring(12);
+                    response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
+                }
+            })
+
         } else {
             response = "HTTP/1.1 404 Not Found\r\n\r\n";
         }
-        
+
         socket.write(response);
     });
 });
