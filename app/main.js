0cbd69ab638482a84bbd4b551f2e4febf0452297
Robledo Pazotto <robledo@gmail.com>
2025-03-11 10:56:54 -0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,13 +1,16 @@
-const net = require("net");
+import {createServer} from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment this to pass the first stage
-const server = net.createServer((socket) => {
+const server = createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
+  
+    socket.on("data", (data) => {
+        console.log("RECEIVED: ", data.toString());
+        
+        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    });
 });
 
 server.listen(4221, "localhost");
+
